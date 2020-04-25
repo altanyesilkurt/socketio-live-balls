16b5fe21783eaf6d31a48c20cf7b81496919d265
altanyesilkurt <altanyesilkurt@cfm.com.tr>
2020-04-25 16:53:49 +0300

    Added: user disconnect event

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,5 +1,5 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
-
+    $scope.messages = [];
     $scope.init = () => {
         const username = prompt('please enter your username');
         if (username)
@@ -14,7 +14,29 @@ app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFacto
             reconnectionDelay: 600
         };
         indexFactory.connectSocket('http://localhost:3000', connectionOptions).then((socket) => {
-            socket.$emit('newUser', {username})
+            socket.emit('newUser', {username})
+            socket.on('newUser', (data) => {
+                const messageData = {
+                    type: {
+                        code: 0,
+                        message: 1
+                    },
+                    username: data.username
+                }
+                $scope.messages.push(messageData);
+                $scope.$apply();
+            })
+            socket.on('disUser', (data) => {
+                const messageData = {
+                    type: {
+                        code: 0,
+                        message: 0
+                    },
+                    username: data.username
+                }
+                $scope.messages.push(messageData);
+                $scope.$apply();
+            })
         }).catch((error) => {
             console.log(error)
         })
--- src/socketApi.js
+++ src/socketApi.js
@@ -4,20 +4,25 @@ const io = socketio();
 const socketApi = {};
 socketApi.io = io;
 
-const users=[]
+const users = {}
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('newUser',(data)=>{
-    const defaultData={
-        id:socket.id,
-        position:{
-            x:0,
-            y:0
+    socket.on('newUser', (data) => {
+        const defaultData = {
+            id: socket.id,
+            position: {
+                x: 0,
+                y: 0
+            }
         }
-    }
-    const userData=Object.assign(data,defaultData);
-    users.push(userData);
+        const userData = Object.assign(data, defaultData);
+        users[socket.id] = userData;
+        socket.broadcast.emit('newUser', userData)
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('disUser', users[socket.id])
+        delete users[socket.id];
     })
 })
 
