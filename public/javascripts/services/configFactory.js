a887d3c21473ff1d59b447b4e1714b1c24abe53d
altan <altanyesilkurt@gmail.com>
2020-05-03 17:41:31 +0300

    Added configFactory

--- public/javascripts/services/configFactory.js
+++ public/javascripts/services/configFactory.js
@@ -1,4 +1,4 @@
-app.factory('configFactory', [() => {
+app.factory('configFactory', ['$http',($http) => {
     const getConfig = () => {
         return new Promise((resolve, reject) => {
             $http.get('/getEnv').then((data) => {
